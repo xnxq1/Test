277e24d09726ba67f23c7434381537f0c8f9f2a5
Vasiliy Prokopev <Vasya3222@inbox.ru>
2024-04-02 13:50:01 +0300

    Add SQL LOGGING

--- Test_task/Test_task/settings.py
+++ Test_task/Test_task/settings.py
@@ -131,3 +131,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'}
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
+}
