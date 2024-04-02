bccd20385a9106159dcd4c3ad00a961614b277f5
Vasiliy Prokopev <Vasya3222@inbox.ru>
2024-04-02 13:48:54 +0300

    Create serializer and set classes

---
+++ Test_task/API/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from API.models import Exercises
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercises
+        fields = '__all__'
--- Test_task/API/views.py
+++ Test_task/API/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from API.serializers import ExerciseSerializer
+from API.models import Exercises
 
 # Create your views here.
+
+
+class ExerciseViewSet(viewsets.ModelViewSet):
+    queryset = Exercises.objects.all()
+    serializer_class = ExerciseSerializer
