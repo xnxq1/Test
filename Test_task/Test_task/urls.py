58749b92136496513d045acad662fe2f1fe0119c
Vasiliy Prokopev <Vasya3222@inbox.ru>
2024-04-03 13:55:39 +0300

    Add JWT Authentication

--- Test_task/Test_task/urls.py
+++ Test_task/Test_task/urls.py
@@ -15,15 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenVerifyView
 from rest_framework import routers
+
 from API.views import ExerciseViewSet
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 router = routers.DefaultRouter()
 router.register(r'exercises', ExerciseViewSet)
-
 urlpatterns += router.urls
 
