e4cf8d9ef55b2c8ccd6df8e1d5ac7b862283bf64
Vasiliy Prokopev <Vasya3222@inbox.ru>
2024-04-03 16:36:52 +0300

    Add filtering

--- Test_task/API/views.py
+++ Test_task/API/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from API.serializers import ExerciseSerializer
 from API.models import Exercises
@@ -10,3 +12,18 @@ from API.models import Exercises
 class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = Exercises.objects.all().prefetch_related('type')
     serializer_class = ExerciseSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+
+    def get_queryset(self):
+        self.queryset = super().get_queryset()
+        type = self.request.GET.get('type', None)
+        difficult = self.request.GET.get('difficult', None)
+
+        if type:
+            self.queryset = self.queryset.filter(type_id__name=type)
+
+        if difficult:
+            self.queryset = self.queryset.filter(difficult=difficult)
+
+        return self.queryset
--- Test_task/Test_task/settings.py
+++ Test_task/Test_task/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 
     'API',
 ]
